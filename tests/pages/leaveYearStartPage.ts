ab4f64b1dc3e576ac13c6a2b68c0267563f26aa4
Thomas Pike <tpike1999@icloud.com>
2026-07-02 13:55:09 +0100

    fix: setup

--- tests/pages/leaveYearStartPage.ts
+++ tests/pages/leaveYearStartPage.ts
@@ -1,46 +1,51 @@
 import { Page, type Locator } from 'playwright';
 import {expect} from "@playwright/test";
-import landingPage_content from "../content/landingPage_content";
+import leaveYearStartPage_content from "../content/leaveYearStartPage_content";
 
-export class LandingPage {
+export class leaveYearStartPage {
     private readonly page: Page;
     private readonly title: string;
     private readonly heading: string;
     private readonly text_line1: string;
-    private readonly text_line2: string;
-    private readonly list_item1: string;
-    private readonly list_item2: string;
-    private readonly start_button: Locator;
-
+    private readonly dayInput: Locator;
+    private readonly monthInput: Locator;
+    private readonly yearInput: Locator;
+    private readonly continue_button: Locator;
+    
     constructor(page: Page) {
         this.page = page;
-        this.title = landingPage_content.pageTitle;
-        this.heading = landingPage_content.heading;
-        this.text_line1 = landingPage_content.text_line1;
-        this.text_line2 = landingPage_content.text_line2;
-        this.list_item1 = landingPage_content.list_item1;
-        this.list_item2 = landingPage_content.list_item2;
-        this.start_button = this.page.locator('a', { hasText: landingPage_content.start_button });
+        this.title = leaveYearStartPage_content.pageTitle;
+        this.heading = leaveYearStartPage_content.heading;
+        this.text_line1 = leaveYearStartPage_content.text_line1;
+        this.dayInput = page.getByLabel('Day');
+        this.monthInput = page.getByLabel('Month');
+        this.yearInput = page.getByLabel('Year');
+        this.continue_button = page.getByRole('button', { name: 'Continue' });
     }
 
     async checkPageLoads(): Promise<void> {
-
-        // Check all elements of the page
         await Promise.all([
-            await expect(this.start_button).toBeVisible(),
             await expect(this.page).toHaveTitle(this.title),
             await expect(this.page.getByRole('heading', { name: this.heading })).toBeVisible(),
             await expect(this.page.getByText(this.text_line1)).toBeVisible(),
-            await expect(this.page.getByText(this.text_line2)).toBeVisible(),
-            await expect(this.page.getByText(this.list_item1)).toBeVisible(),
-            await expect(this.page.getByText(this.list_item2)).toBeVisible(),
+            await expect(this.dayInput).toBeVisible(),
+            await expect(this.monthInput).toBeVisible(),
+            await expect(this.yearInput).toBeVisible(),
+            await expect(this.continue_button).toBeVisible(),
         ]);
     }
 
+    async enterDate(date: string): Promise<void> {
+        const [day, month, year] = date.split("/")
+        await this.dayInput.fill(day);
+        await this.monthInput.fill(month);
+        await this.yearInput.fill(year);
+    }
+
     async continueOn(): Promise<void> {
-        // Click the continue button
-        await this.start_button.click();
+        await this.continue_button.click();
     }
 }
+    
 
-export default LandingPage;
+export default leaveYearStartPage;
